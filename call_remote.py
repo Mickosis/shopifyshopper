6f42f1257830bd95fb145304fec2f14fbc2a6eb7
Mico Rigunay <mico.rigunay@gmail.com>
2024-10-02 12:12:38 +0800

    Update call_remote.py

--- call_remote.py
+++ call_remote.py
@@ -7,7 +7,6 @@ Created by Mickosis on Mon Mar 1 04:20:00 2021
 
 import socket
 import requests
-import json
 import time
 from twilio.rest import Client
 from dhooks import Webhook
@@ -33,60 +32,55 @@ CONFIG = {
 hook = Webhook(CONFIG["discord_webhook"])
 aws_hook = Webhook(CONFIG["aws_webhook"])
 
-# Functions
+def notify(message):
+    """Send a notification message via Discord webhook and print to console."""
+    print(message)
+    hook.send(message)
 
 def find_wanted_product(products):
-    """Checks if any of the wanted products are available in the product list."""
-    for item in CONFIG["wanted_items"]:
-        for product in products:
-            if item == product["title"]:
-                return product["title"]
-    return False
+    """Check if any of the wanted products are available in the product list."""
+    return next((product["title"] for product in products if product["title"] in CONFIG["wanted_items"]), None)
 
 def call_user(client):
-    """Attempts to call the user via Twilio."""
+    """Attempt to call the user via Twilio multiple times."""
     for attempt in range(1, 6):
         call = client.calls.create(
             to=CONFIG["twilio"]["to_phone"],
             from_=CONFIG["twilio"]["from_phone"],
             url=CONFIG["twilio"]["voice_url"]
         )
-        message = f"[{attempt}] Attempt to call {CONFIG['twilio']['to_phone']} with SID {call.sid}"
-        print(message)
-        hook.send(message)
+        notify(f"[{attempt}] Attempt to call {CONFIG['twilio']['to_phone']} with SID {call.sid}")
         time.sleep(CONFIG["standby_interval"])
 
-# Main Program
-
-def main():
-    host_name = socket.gethostname()
-    print(f"=== MONITOR INITIATED ON {host_name} ===")
-    hook.send(f"=== MONITOR INITIATED ON {host_name} ===")
-    
-    client = Client(CONFIG["twilio"]["account_sid"], CONFIG["twilio"]["auth_token"])
-    
+def check_products(client):
+    """Check the availability of wanted products and notify accordingly."""
     while True:
         try:
             current_time = datetime.now().strftime("%H:%M:%S")
             response = requests.get(CONFIG["json_url"])
-            products = response.json()["products"]
+            products = response.json().get("products", [])
             product_name = find_wanted_product(products)
-            
+
             if product_name:
-                message = f"[{current_time}]: Product {product_name} is available!"
-                print(message)
-                hook.send(message)
-                aws_hook.send(message)
+                notify(f"[{current_time}]: Product {product_name} is available!")
+                aws_hook.send(f"[{current_time}]: Product {product_name} is available!")
                 call_user(client)
                 break
             else:
-                print(f"[{current_time}] [{len(products)} products]: Not available.")
+                notify(f"[{current_time}] [{len(products)} products]: Not available.")
                 aws_hook.send(f"[{current_time}] [{len(products)} products]: Not available.")
                 time.sleep(CONFIG["standby_interval"])
         except KeyboardInterrupt:
-            print(f"=== MONITOR INTERRUPTED ON {host_name} ===")
-            hook.send(f"=== MONITOR INTERRUPTED ON {host_name} ===")
+            host_name = socket.gethostname()
+            notify(f"=== MONITOR INTERRUPTED ON {host_name} ===")
             break
+
+def main():
+    host_name = socket.gethostname()
+    notify(f"=== MONITOR INITIATED ON {host_name} ===")
+    
+    client = Client(CONFIG["twilio"]["account_sid"], CONFIG["twilio"]["auth_token"])
+    check_products(client)
 
 if __name__ == "__main__":
     main()
